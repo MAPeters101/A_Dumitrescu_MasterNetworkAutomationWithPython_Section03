ddec12092de84b723002dbff587753ec7be57810
Mark Peters <mapeters101@earthlink.net>
2024-11-16 13:49:37 -0500

    Lesson028:  Work on lesson to 5:40.

---
+++ lesson028.py
@@ -0,0 +1,8 @@
+
+#f = open('settings.txt')
+#f = open('E:\Documents\Udemy\A_Dumitrescu\MasterNetworkAutomationWithPython\Section003\pythonLessons\settings.txt')
+f = open('E:\\Documents\\Udemy\\A_Dumitrescu\\MasterNetworkAutomationWithPython\\Section003\\pythonLessons\\settings.txt')
+
+
+
+
